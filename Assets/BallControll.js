49a325b218f90d0f24fc02be3219f453bd3c9484
wataru0225 <wataru.kikuchi0225@gmail.com>
2015-09-02 13:59:17 +0900

    first commit

---
+++ Assets/BallControll.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+function Update () {
+	var horizMove : float = Input.GetAxis("Horizontal");
+	transform.position += Vector3(horizMove * 0.1, 0, 0);
+
+	var vertMove : float = Input.GetAxis("Vertical");
+	transform.position += Vector3(0, 0, vertMove * 0.1);
+}
---
+++ Assets/PlateScript.js
@@ -0,0 +1,8 @@
+#pragma strict
+
+var enemy : Transform;
+
+function Update () {
+	if (Time.frameCount % 60 == 0) {
+	}
+}

fc4491abc44b79d18f45fd0185ae31736ec38131
wataru0225 <wataru.kikuchi0225@gmail.com>
2015-09-03 23:20:46 +0900

    [add]カメラを追従するようにする（くるくるしないようになった。）

---
+++ Assets/CameraScript.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+var obj : GameObject;
+var offset : Vector3;
+
+function Update () {
+	var position : Vector3;
+    position = obj.transform.localPosition;
+    transform.localPosition = position + offset;
+}

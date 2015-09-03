a633a37f3151777e1a6c013472bc4a1f606380e4
wataru0225 <wataru.kikuchi0225@gmail.com>
2015-09-04 00:34:10 +0900

    [add]いろいろassetを追加（audio, skybox）

--- Assets/GameOver.js
+++ Assets/GameOver.js
@@ -1,10 +1,5 @@
 ﻿var style : GUIStyle;
 
 function OnGUI() {
-  GUI.Label(Rect(10,10,100,30), "Game Over", style);
-  Invoke("Restart", 3);
-}
-
-function Restart() {
-	Application.LoadLevel("main");
+	GUI.Label(Rect(10,10,100,30), "Game Over", style);
 }

1f62c2b9853dd17a6451a9454e639587ce52f4d6
wataru0225 <wataru.kikuchi0225@gmail.com>
2015-09-03 22:12:23 +0900

    [add]プレートをプログラムで生成する

--- Assets/PlateScript.js
+++ Assets/PlateScript.js
@@ -1,8 +1,37 @@
 #pragma strict
 
 var enemy : Transform;
+var i : int;
+var k : int;
+var z : int;
 
 function Update () {
-	if (Time.frameCount % 60 == 0) {
+	if (i < 20) {
+		if (Time.frameCount % 10 == 0) {
+			i++;
+			// 素数だったら
+			if (checkPrime(i)) {
+				Instantiate(enemy, Vector3(5 * (k - 1), 0, 5 * z), transform.rotation);
+				z++;
+			} else {
+				Instantiate(enemy, Vector3(5 * k, 0, 5 * z), transform.rotation);
+				k++;
+			}
+		}
 	}
+}
+
+
+// 素数をチェック
+private function checkPrime(i : int) {
+	if (i > 2) {
+		for (var j = 2; j < i; j++) {
+			if (i % j == 0) {
+				return false;
+			}
+		}
+	} else {
+		return false;
+	}
+	return true;
 }

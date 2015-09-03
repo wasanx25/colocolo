cbb13ff7c4043104bad74dcf24e795a176ba0105
wataru0225 <wataru.kikuchi0225@gmail.com>
2015-09-03 23:38:53 +0900

    [add]GameOver Action (アニメーションとかなし)

--- Assets/BallControll.js
+++ Assets/BallControll.js
@@ -1,9 +1,15 @@
 ﻿#pragma strict
 
 function Update () {
+	// pc用
 	var horizMove : float = Input.GetAxis("Horizontal");
 	transform.position += Vector3(horizMove * 0.1, 0, 0);
 
+	// sp用
 	var vertMove : float = Input.GetAxis("Vertical");
 	transform.position += Vector3(0, 0, vertMove * 0.1);
+
+	if (transform.position.y < -10) {
+		Application.LoadLevel("GameOver");
+	}
 }
---
+++ Assets/GameOver.js
@@ -0,0 +1,10 @@
+﻿var style : GUIStyle;
+
+function OnGUI() {
+  GUI.Label(Rect(10,10,100,30), "Game Over", style);
+  Invoke("Restart", 3);
+}
+
+function Restart() {
+	Application.LoadLevel("main");
+}

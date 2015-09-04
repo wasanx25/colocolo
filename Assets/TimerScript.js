bdfbc4c9c626523b45b3b266121aff10fdeedcef
wataru0225 <wataru.kikuchi0225@gmail.com>
2015-09-04 13:30:48 +0900

    [add]タイマーを表示

---
+++ Assets/TimerScript.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+import UnityEngine.UI;
+
+var time : float;
+
+function Update () {
+	time += Time.deltaTime;
+	var t : int = Mathf.FloorToInt(time);
+	var uiText : Text = GetComponent.<Text>();
+	uiText.text = "Time : " + t.ToString();
+}

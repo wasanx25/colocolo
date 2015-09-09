7327ca49b16c3071c5b2ad09f0af22b626170b00
wataru0225 <wataru.kikuchi0225@gmail.com>
2015-09-09 20:43:05 +0900

    [add]スタート時の効果音を追加

--- Assets/StartScript.js
+++ Assets/StartScript.js
@@ -1,8 +1,11 @@
 ﻿#pragma strict
 
+var sound : AudioClip;
+
 function Update () {
 	for (var touch : Touch in Input.touches) {
 		if (touch.phase == TouchPhase.Began) {
+			GetComponent.<AudioSource>().Play();
 			Application.LoadLevel("Main");
 		}
 	}
